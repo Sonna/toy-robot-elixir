82be2ba29f062d40d3b77de1be6b9f471e80abe4
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-08 22:58:52 +1100

    Fix Robot move
    Due to a misunderstanding `when` guard conditionals and how the `move`
    function was written, this fix simplifies it to a regular if else
    conditional that returns an updated Robot depending on the robot's
    position if its at the edge of the table and its facing direction.
    
    == References:
    - [case, cond, and if \- Elixir]
      (https://elixir-lang.org/getting-started/case-cond-and-if.html)

--- lib/toy_robot_elixir.ex
+++ lib/toy_robot_elixir.ex
@@ -37,41 +37,22 @@ defmodule ToyRobot do
     def left(robot, _ \\ ""), do: %{robot | facing: @turn[robot.facing]["LEFT"]}
     def right(robot, _ \\ ""), do: %{robot | facing: @turn[robot.facing]["RIGHT"]}
 
-    def move(robot, _ \\ "")
+    # def move(robot, _ \\ "")
 
-    def move(%Robot{x: x, facing: facing} = robot, _) when x >= 0 and facing != "WEST" do
-      %{robot | x: robot.x + @move[robot.facing][:x], y: robot.y + @move[robot.facing][:y] }
+    # def move(%Robot{x: x, facing: facing} = robot, _) when x >= 0 and facing != "WEST", do: _move(robot)
+    # def move(%Robot{x: x, facing: facing} = robot, _) when x <= 4 and facing != "EAST", do: _move(robot)
+    # def move(%Robot{y: y, facing: facing} = robot, _) when y >= 0 and facing != "SOUTH", do: _move(robot)
+    # def move(%Robot{y: y, facing: facing} = robot, _) when y <= 4 and facing != "NORTH", do: _move(robot)
+
+    def move(%Robot{x: x, y: y, facing: facing} = robot, _ \\ "") do
+      cond do
+        (y == 0 and facing == "SOUTH") or (y == 4 and facing == "NORTH") ->
+          %{robot | x: x + @move[facing][:x] }
+        (x == 0 and facing == "WEST") or (x == 4 and facing == "EAST") ->
+          %{robot | y: y + @move[facing][:y] }
+        true ->
+          %{robot | x: x + @move[facing][:x], y: y + @move[facing][:y] }
+      end
     end
-
-    # def move(robot, _ \\ "") when robot.x < 4 and robot.facing != "EAST" do
-    def move(%Robot{x: x, facing: facing} = robot, _) when x <= 4 and facing != "EAST" do
-      %{robot | x: robot.x + @move[robot.facing][:x], y: robot.y + @move[robot.facing][:y] }
-    end
-
-    # def move(robot, _ \\ "") when robot.y > 0 and robot.facing != "SOUTH" do
-    def move(%Robot{y: y, facing: facing} = robot, _) when y >= 0 and facing != "SOUTH" do
-      %{robot | x: robot.x + @move[robot.facing][:x], y: robot.y + @move[robot.facing][:y] }
-    end
-
-    # def move(robot, _ \\ "") when robot.y < 4 and robot.facing != "NORTH" do
-    def move(%Robot{y: y, facing: facing} = robot, _) when y <= 4 and facing != "NORTH" do
-      %{robot | x: robot.x + @move[robot.facing][:x], y: robot.y + @move[robot.facing][:y] }
-    end
-
-    # def move(robot, _ \\ "") do
-    #   new_robot = %{robot |
-    #     x: robot.x + @move[robot.facing][:x],
-    #     y: robot.y + @move[robot.facing][:y]
-    #   }
-
-    #   if (new_robot.x < 0 || new_robot.x >= 4) do
-    #     new_robot = %{new_robot | x: robot.x - @move[robot.facing][:x]}
-    #   end
-
-    #   if (new_robot.y < 0 || new_robot.y >= 4) do
-    #     new_robot = %{new_robot | y: robot.y - @move[robot.facing][:y]}
-    #   end
-    #   new_robot
-    # end
   end
 end
--- test/robot_elixir_test.exs
+++ test/robot_elixir_test.exs
@@ -26,30 +26,118 @@ defmodule RobotTest do
     assert subject.facing == "NORTH"
   end
 
-  test "default Robot report " do
+  test "default Robot report" do
     subject = %ToyRobot.Robot{}
     assert capture_io(
       fn -> ToyRobot.Robot.report(subject) end
     ) == "0, 0, NORTH\n"
   end
 
-  test "turn Robot left " do
+  test "turn Robot left" do
     subject = %ToyRobot.Robot{}
     new_subject = ToyRobot.Robot.left(subject)
     assert new_subject.facing == "WEST"
   end
 
-  test "turn Robot right " do
+  test "turn Robot right" do
     subject = %ToyRobot.Robot{}
     new_subject = ToyRobot.Robot.right(subject)
     assert new_subject.facing == "EAST"
   end
 
-  test "move Robot to 0,1,NORTH " do
+  test "move Robot to 0,1,NORTH" do
     subject = %ToyRobot.Robot{}
     new_subject = ToyRobot.Robot.move(subject)
     assert new_subject.x == 0
     assert new_subject.y == 1
     assert new_subject.facing == "NORTH"
+  end
+
+  test "move Robot to 1,0,EAST" do
+    subject = %ToyRobot.Robot{facing: "EAST"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 1
+    assert new_subject.y == 0
+    assert new_subject.facing == "EAST"
+  end
+
+  test "move Robot from 0,3,SOUTH to 0,2,SOUTH" do
+    subject = %ToyRobot.Robot{y: 3, facing: "SOUTH"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 0
+    assert new_subject.y == 2
+    assert new_subject.facing == "SOUTH"
+  end
+
+  test "move Robot from 2,1,WEST to 1,1,WEST" do
+    subject = %ToyRobot.Robot{x: 2, y: 1, facing: "WEST"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 1
+    assert new_subject.y == 1
+    assert new_subject.facing == "WEST"
+  end
+
+  test "Robot does not move off the table from 0,4,NORTH" do
+    subject = %ToyRobot.Robot{x: 0, y: 4, facing: "NORTH"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 0
+    assert new_subject.y == 4
+    assert new_subject.facing == "NORTH"
+  end
+
+  test "Robot does not move off the table from 4,4,NORTH" do
+    subject = %ToyRobot.Robot{x: 4, y: 4, facing: "NORTH"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 4
+    assert new_subject.y == 4
+    assert new_subject.facing == "NORTH"
+  end
+
+  test "Robot does not move off the table from 0,0,SOUTH" do
+    subject = %ToyRobot.Robot{x: 0, y: 0, facing: "SOUTH"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 0
+    assert new_subject.y == 0
+    assert new_subject.facing == "SOUTH"
+  end
+
+  test "Robot does not move off the table from 4,0,SOUTH" do
+    subject = %ToyRobot.Robot{x: 4, y: 0, facing: "SOUTH"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 4
+    assert new_subject.y == 0
+    assert new_subject.facing == "SOUTH"
+  end
+
+  test "Robot does not move off the table from 0,0,WEST" do
+    subject = %ToyRobot.Robot{x: 0, y: 0, facing: "WEST"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 0
+    assert new_subject.y == 0
+    assert new_subject.facing == "WEST"
+  end
+
+  test "Robot does not move off the table from 0,4,WEST" do
+    subject = %ToyRobot.Robot{x: 0, y: 4, facing: "WEST"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 0
+    assert new_subject.y == 4
+    assert new_subject.facing == "WEST"
+  end
+
+  test "Robot does not move off the table from 4,0,EAST" do
+    subject = %ToyRobot.Robot{x: 4, y: 0, facing: "EAST"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 4
+    assert new_subject.y == 0
+    assert new_subject.facing == "EAST"
+  end
+
+  test "Robot does not move off the table from 4,4,EAST" do
+    subject = %ToyRobot.Robot{x: 4, y: 4, facing: "EAST"}
+    new_subject = ToyRobot.Robot.move(subject)
+    assert new_subject.x == 4
+    assert new_subject.y == 4
+    assert new_subject.facing == "EAST"
   end
 end
