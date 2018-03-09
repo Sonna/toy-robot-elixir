3a3935e17f0bf9300df95d4981e2e4c78b9b773a
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-09 11:55:19 +1100

    Remove unnecessary Hello World function

--- lib/toy_robot_elixir.ex
+++ lib/toy_robot_elixir.ex
@@ -1,21 +1,4 @@
 defmodule ToyRobot do
-  @moduledoc """
-  Documentation for ToyRobot.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ToyRobot.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   defmodule Robot do
     defstruct x: 0, y: 0, facing: "NORTH"
 
